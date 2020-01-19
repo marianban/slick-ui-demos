84d4bb523787dc6dbac9408e1a27ddadfb398816
Marian Ban <marian.ban@gmail.com>
2020-01-19 12:23:20 +0100

    last days history

--- smart-home-app/src/App.js
+++ smart-home-app/src/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import './App.scss';
+import { History } from './History';
 
 function App() {
   return (
     <div className="app">
       <div className="menu">
         <div className="menu-item menu-item--active">
-          <i class="fas fa-home"></i>
+          <i className="fas fa-home"></i>
         </div>
         <div className="menu-item">
-          <i class="fas fa-th-large"></i>
+          <i className="fas fa-th-large"></i>
         </div>
         <div className="menu-item">
-          <i class="fas fa-cog"></i>
+          <i className="fas fa-cog"></i>
         </div>
         <div className="menu-item">
-          <i class="fas fa-filter"></i>
+          <i className="fas fa-filter"></i>
         </div>
         <div className="menu-item menu-item__section">
-          <i class="fas fa-map-marker"></i>
+          <i className="fas fa-map-marker"></i>
         </div>
         <div className="menu-item">
-          <i class="fas fa-male"></i>
+          <i className="fas fa-male"></i>
         </div>
         <div className="menu-item">
-          <i class="fas fa-folder-open"></i>
+          <i className="fas fa-folder-open"></i>
         </div>
         <div className="menu__avatar">
           <img src="https://i.picsum.photos/id/838/384/384.jpg" alt="avatar" />
@@ -33,51 +34,51 @@ function App() {
       <div className="main">
         <div className="header">
           <div className="header__back">
-            <i class="fas fa-chevron-left"></i>
+            <i className="fas fa-chevron-left"></i>
           </div>
           <div className="header__subtitle">Global</div>
           <h1 className="header__title">Temperature</h1>
           <div className="header__humidity header__control">
-            <i class="fas fa-tint"></i>
+            <i className="fas fa-tint"></i>
             53%
           </div>
           <div className="header__ac header__control">
-            <i class="fas fa-fan"> </i>
-            <i class="fas fa-toggle-on"></i>
+            <i className="fas fa-fan"> </i>
+            <i className="fas fa-toggle-on"></i>
           </div>
           <div className="header__more header__control">
-            <i class="fas fa-ellipsis-h"></i>
+            <i className="fas fa-ellipsis-h"></i>
           </div>
         </div>
         <div className="control"></div>
-        <div className="history"></div>
+        <History />
       </div>
       <div className="sidebar">
         <h2>Shortcuts</h2>
         <label className="shortcut">
           <button className="btn btn--1">
-            <i class="fas fa-thermometer-three-quarters"></i>
+            <i className="fas fa-thermometer-three-quarters"></i>
           </button>
           <span className="title">Temperature</span>
           <span className="text">24C</span>
         </label>
         <label className="shortcut">
           <button className="btn btn--2">
-            <i class="fas fa-wifi"></i>
+            <i className="fas fa-wifi"></i>
           </button>
           <span className="title">Internet</span>
           <span className="text">78.22</span>
         </label>
         <label className="shortcut">
           <button className="btn btn--3">
-            <i class="far fa-lightbulb"></i>
+            <i className="far fa-lightbulb"></i>
           </button>
           <span className="title">Lights</span>
           <span className="text">9+ 4+</span>
         </label>
         <label className="shortcut">
           <button className="btn btn--plus">
-            <i class="fas fa-plus"></i>
+            <i className="fas fa-plus"></i>
           </button>
           <span className="title"></span>
           <span className="text"></span>
@@ -85,19 +86,19 @@ function App() {
         <h2>Devices</h2>
         <div className="devices">
           <button className="btn">
-            <i class="fas fa-tv"></i>
+            <i className="fas fa-tv"></i>
           </button>
           <button className="btn">
-            <i class="fas fa-video"></i>
+            <i className="fas fa-video"></i>
           </button>
           <button className="btn">
-            <i class="fas fa-bath"></i>
+            <i className="fas fa-bath"></i>
           </button>
           <button className="btn">
-            <i class="far fa-hdd"></i>
+            <i className="far fa-hdd"></i>
           </button>
           <button className="btn btn--plus">
-            <i class="fas fa-plus"></i>
+            <i className="fas fa-plus"></i>
           </button>
         </div>
       </div>
---
+++ smart-home-app/src/History.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import * as d3 from 'd3';
+import v4 from 'uuid';
+
+const data = Array.from({ length: 13 }, (_, i) => ({
+  id: v4(),
+  day: 19 + i,
+  temperature: 10 + Math.round(Math.random() * 20)
+}));
+
+console.table(data);
+
+export const History = () => {
+  const yAccessor = d => d.temperature;
+  const boundedHeight = 150;
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, yAccessor)])
+    .range([0, boundedHeight])
+    .nice();
+
+  const extent = d3.extent(data, yAccessor);
+
+  const colorScale = d3
+    .scaleLinear()
+    .domain(extent)
+    .range(['#F1F0F5', '#D3D0E1']);
+
+  const barColorScale = d => {
+    const [min, max] = extent;
+    const y = yAccessor(d);
+    if (y === min) {
+      return '#6F32FF';
+    }
+    if (y === max) {
+      return '#FF9066';
+    }
+    return colorScale(y);
+  };
+
+  return (
+    <div className="history">
+      <h2>Last Days</h2>
+      <div className="bar-chart">
+        {data.map(d => (
+          <>
+            <div
+              key={d.id}
+              className="bar-chart__bar"
+              style={{
+                height: yScale(yAccessor(d)),
+                backgroundColor: barColorScale(d)
+              }}
+            />
+            <div className="bar-chart__bar__label">{d.day}</div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};

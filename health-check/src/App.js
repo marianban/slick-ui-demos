8117f5af168778c85108d154203385aa05ec0e8e
Marian Ban <marian.ban@gmail.com>
2020-02-17 21:49:01 +0100

    cube height animation

--- health-check/src/App.js
+++ health-check/src/App.js
@@ -4,15 +4,7 @@ import './App.scss';
 function App() {
   return (
     <div className="app">
-      <div className="cube">
-        <div className="side back">back</div>
-        <div className="side left">left</div>
-        <div className="side right">right</div>
-        <div className="side top">top</div>
-        <div className="side bottom">bottom</div>
-        <div className="side front">front</div>
-      </div>
-      <div className="cube-reflection">
+      <div className="cube-container">
         <div className="cube">
           <div className="side back">back</div>
           <div className="side left">left</div>
@@ -20,6 +12,16 @@ function App() {
           <div className="side top">top</div>
           <div className="side bottom">bottom</div>
           <div className="side front">front</div>
+        </div>
+        <div className="cube-reflection">
+          <div className="cube">
+            <div className="side back reflection">back</div>
+            <div className="side left reflection">left</div>
+            <div className="side right reflection">right</div>
+            <div className="side top reflection">top</div>
+            <div className="side bottom reflection">bottom</div>
+            <div className="side front reflection">front</div>
+          </div>
         </div>
       </div>
     </div>

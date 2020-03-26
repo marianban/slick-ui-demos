7082f65f768be267a0f48c4e3b704aa68d66ce49
Marian Ban <marian.ban@gmail.com>
2020-03-26 21:33:50 +0100

    small cleanup

--- health-check/src/Cube.js
+++ health-check/src/Cube.js
@@ -4,20 +4,16 @@ import './Cube.scss';
 export const Cube = ({ style = {}, className = '' }) => (
   <div className={`cube-container ${className}`} style={style}>
     <div className="cube actual">
-      <div className="side back">back</div>
       <div className="side left">left</div>
       <div className="side right">right</div>
       <div className="side top">top</div>
-      <div className="side bottom">bottom</div>
       <div className="side front">front</div>
     </div>
 
     <div className="cube reflection">
-      <div className="side back reflection">back</div>
       <div className="side left reflection">left</div>
       <div className="side right reflection">right</div>
       <div className="side top reflection">top</div>
-      <div className="side bottom reflection">bottom</div>
       <div className="side front reflection">front</div>
     </div>
   </div>

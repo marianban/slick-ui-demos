d806779c5a6f3fcfb40fac22844279cffa47edde
Marian Ban <marian.ban@gmail.com>
2020-12-12 16:26:24 +0100

    flow field

--- flow-field/index.js
+++ flow-field/index.js
@@ -16,11 +16,43 @@ class Ant {
 
   update() {
     this.velocity.add(this.acceleration);
+    this.velocity.limit(5);
+    this.location.add(this.velocity);
     this.acceleration.mult(0);
+  }
+
+  within(element) {
+    const { x, y } = this.location;
+    if (
+      element.x < x &&
+      x < element.x + element.size &&
+      element.y < y &&
+      y < element.y + element.size
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  bounds() {
+    const { x, y } = this.location;
+    if (x > width) {
+      this.location.x = 0;
+    }
+    if (x < 0) {
+      this.location.x = width;
+    }
+    if (y > height) {
+      this.location.y = 0;
+    }
+    if (y < 0) {
+      this.location.y = height;
+    }
   }
 
   draw() {
     push();
+    fill('#000000');
     translate(this.location.x, this.location.y);
     circle(0, 0, 5);
     pop();
@@ -51,6 +83,7 @@ function draw() {
   let yoff = 0;
   let xoff = 82300;
   toff += timeStep;
+  forces.length = 0;
 
   translate(-width / 2, -height / 2, 0);
   for (let r = 0; r <= rows; r++) {
@@ -60,13 +93,16 @@ function draw() {
     for (let c = 0; c <= cols; c++) {
       let x = c * size;
       xoff += noiseStep;
-      const n = noise(yoff, xoff);
+      const n = noise(yoff, xoff, toff);
       const color = n * 255;
       const angle = n * Math.PI * 2;
       const length = 1;
       const vector = createVector(cos(angle), sin(angle));
       vector.mult(size / 2);
-      forces.push(vector);
+      const force = vector.copy();
+      force.normalize();
+      force.mult(0.1);
+      forces.push({ force, x, y, size });
       fill(color);
       noStroke();
       rect(x, y, size);
@@ -76,6 +112,20 @@ function draw() {
       line(0, 0, vector.x, vector.y);
       pop();
     }
+  }
+
+  for (let i = 0; i < ants.length; i++) {
+    const ant = ants[i];
+    for (let j = 0; j < forces.length; j++) {
+      const box = forces[j];
+      if (ant.within(box)) {
+        const { force } = box;
+        ant.applyForce(force);
+      }
+    }
+    ant.update();
+    ant.bounds();
+    ant.draw();
   }
 }
 

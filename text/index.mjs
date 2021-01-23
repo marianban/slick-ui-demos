248afedbeeb6bd6a6fa09750d22df96207ab6180
Marian Ban <marian.ban@gmail.com>
2021-01-23 20:39:08 +0100

    text effect

--- text/index.mjs
+++ text/index.mjs
@@ -1,6 +1,8 @@
 const sketch = function (p) {
   let font;
-  const vehicles = [];
+  let text = 'hello';
+  let letters;
+  let deletedLetters = new Set();
 
   p.preload = () => {
     font = p.loadFont('HachiMaruPop-Regular.ttf');
@@ -10,17 +12,10 @@ const sketch = function (p) {
     const canvas = p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
     canvas.parent('container');
 
-    const points = font.textToPoints('hello', 400, 400, 255);
+    const allPoints = font.textToPoints(text, 400, 400, 255);
+    const letterPointGroups = groupPointsByLetters(allPoints.reverse());
 
-    for (let i = 0; i < points.length; i++) {
-      const pt = points[i];
-      vehicles.push(
-        new Vehicle(
-          p.createVector(p.random(p.windowWidth), p.random(p.windowHeight)),
-          p.createVector(pt.x, pt.y)
-        )
-      );
-    }
+    letters = letterPointGroups.map((points) => new Letter(points));
   };
 
   p.windowResized = () => {
@@ -29,16 +24,113 @@ const sketch = function (p) {
 
   p.draw = () => {
     p.translate(-p.windowWidth / 2, -p.windowHeight / 2);
-    // p.background(51);
     p.clear();
-    vehicles.forEach((v) => {
-      v.behaviour();
-      v.update();
-      v.draw();
+    letters.forEach((letter) => {
+      letter.behaviour();
+      letter.update();
+      letter.draw();
+    });
+    Array.from(deletedLetters).forEach((letter) => {
+      letter.behaviour();
+      letter.update();
+      letter.draw();
+      if (letter.isOutside()) {
+        deletedLetters.delete(letter);
+      }
     });
   };
 
-  class Vehicle {
+  p.keyPressed = () => {
+    if (p.keyCode === p.BACKSPACE && letters.length) {
+      const deleted = letters.pop();
+      deleted.delete();
+      deletedLetters.add(deleted);
+    }
+    return false; // prevent any default behaviour
+  };
+
+  function groupPointsByLetters(points) {
+    const letters = [];
+    let prev = points.pop();
+    let letter = [prev];
+    let next;
+    while ((next = points.pop())) {
+      const distanceX = next.x - prev.x;
+      const distanceY = next.y - prev.y;
+      const euclideanDistance = Math.sqrt(distanceX ** 2 + distanceY ** 2);
+      if (euclideanDistance >= 70) {
+        letters.push(letter.slice());
+        letter = [];
+      }
+      letter.push(next);
+      prev = next;
+    }
+    letters.push(letter);
+    return letters;
+  }
+
+  class Letter {
+    constructor(points) {
+      this.points = points.map(
+        (point) =>
+          new Point(
+            p.createVector(p.random(p.windowWidth), p.random(p.windowHeight)),
+            p.createVector(point.x, point.y)
+          )
+      );
+      this.deleted = false;
+    }
+    draw() {
+      this.points.forEach((point) => {
+        point.draw();
+      });
+    }
+    behaviour() {
+      this.points.forEach((point) => {
+        point.behaviour();
+      });
+      if (this.deleted) {
+        this.points.forEach((point) => {
+          const force = point.flee(this.centroid);
+          point.applyForce(force);
+        });
+      }
+    }
+    delete() {
+      const xSum = this.points.reduce((sum, p) => {
+        return sum + p.pos.x;
+      }, 0);
+      const ySum = this.points.reduce((sum, p) => {
+        return sum + p.pos.y;
+      }, 0);
+      const x = xSum / this.points.length;
+      const y = ySum / this.points.length;
+      const centroid = p.createVector(x, y);
+      this.centroid = centroid;
+
+      this.deleted = true;
+    }
+    update() {
+      this.points.forEach((point) => {
+        point.update();
+      });
+    }
+    isOutside() {
+      return this.points.every((p) => isOutsideOfScreen(p.pos));
+    }
+  }
+
+  function isOutsideOfScreen(pos) {
+    if (pos.x < 0 || pos.x > p.width) {
+      return true;
+    }
+    if (pos.y < 0 || pos.y > p.height) {
+      return true;
+    }
+    return false;
+  }
+
+  class Point {
     constructor(pos, target = p.createVector()) {
       this.pos = pos;
       this.target = target;
@@ -57,9 +149,9 @@ const sketch = function (p) {
     flee(target) {
       const desired = p5.Vector.sub(target, this.pos);
       const distance = desired.mag();
-      if (distance > 150) {
-        return p.createVector(0, 0);
-      }
+      // if (distance > 150) {
+      //   return p.createVector(0, 0);
+      // }
       desired.normalize();
       desired.mult(p.random(this.maxSpeed / 2, this.maxSpeed * 2));
       const steer = p5.Vector.sub(desired, this.vel);
@@ -85,11 +177,6 @@ const sketch = function (p) {
     behaviour() {
       const force = this.arrive(this.target);
       this.applyForce(force);
-      if (p.mouseIsPressed) {
-        const mouse = p.createVector(p.mouseX, p.mouseY);
-        const fleeForce = this.flee(mouse);
-        this.applyForce(fleeForce);
-      }
     }
 
     applyForce(force) {

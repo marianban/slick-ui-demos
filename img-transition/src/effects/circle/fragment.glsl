03211055e6cd5e65dbbbb3decf94df9d3229d99a
Marian Ban <marian.ban@gmail.com>
2022-05-28 16:30:46 +0200

    circle effect

---
+++ img-transition/src/effects/circle/fragment.glsl
@@ -0,0 +1,20 @@
+varying vec2 vUv;
+
+uniform sampler2D uTexture1;
+uniform sampler2D uTexture2;
+uniform float uProgress;
+uniform float uProgressMix;
+uniform vec2 uScreenSize;
+uniform float uAspectRatio;
+
+void main()
+{
+    vec2 nUv = vUv;
+
+    vec4 color1 = texture(uTexture1, nUv);
+    vec4 color2 = texture(uTexture2, nUv);
+    float dist = length((nUv - 0.5));
+    float s = smoothstep(dist - dist * 0.2, dist, uProgressMix);
+    vec4 color = mix(color1, color2, clamp(s, 0., 1.));
+    gl_FragColor = color;
+}

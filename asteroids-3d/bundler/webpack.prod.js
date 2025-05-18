722f365f14e0d9ef4965a456940167cacf59aa73
Marian Ban <marian.ban@gmail.com>
2025-05-18 14:58:40 +0200

    fix base url

--- asteroids-3d/bundler/webpack.prod.js
+++ asteroids-3d/bundler/webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
 
 module.exports = merge(
     commonConfiguration,
@@ -8,7 +10,14 @@ module.exports = merge(
         mode: 'production',
         plugins:
         [
-            new CleanWebpackPlugin()
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, '../src/index.html'),
+                minify: true,
+                'base': {
+                    'href': process.env.NODE_ENV === 'development' ? '/' : '/asteroids-3d/dist'
+                }
+            }),
         ]
     }
 )
